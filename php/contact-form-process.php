4fd736acb3e016c0537868c893421e90c9b4243e
AlexandreStang <lealexandrestang@gmail.com>
2022-03-17 19:14:18 -0400

    Added PHP Contact form

---
+++ php/contact-form-process.php
@@ -0,0 +1,11 @@
+<?php 
+$name = $_POST['visitor_name'];
+$email = $_POST['visitor_email'];
+$message = $_POST['visitor_message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "lealexandrestang@gmail.com";
+$subject = "Portfolio Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Merci de votre message! Je reviendrai à vous dans les plus brefs délais!";
+?>
