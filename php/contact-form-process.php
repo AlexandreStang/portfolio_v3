d4333b1c7bee0a0367d4e7f578a5fe0a2e008cf2
AlexandreStang <lealexandrestang@gmail.com>
2022-03-19 20:06:39 -0400

    Tweaked the email and the CV

--- php/contact-form-process.php
+++ php/contact-form-process.php
@@ -3,7 +3,7 @@ $name = $_POST['visitor_name'];
 $email = $_POST['visitor_email'];
 $message = $_POST['visitor_message'];
 $formcontent="De: $name \n Message: $message";
-$recipient = "lealexandrestang@gmail.com";
+$recipient = "alexandre.stang.web@gmail.com";
 $subject = "Portfolio Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
