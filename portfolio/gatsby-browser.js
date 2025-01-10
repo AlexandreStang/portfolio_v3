04a9faaa529ec51b3afb67af557b2e96826258f7
AlexandreStang <91079041+AlexandreStang@users.noreply.github.com>
2025-01-10 16:01:11 -0500

    Fixed AOS animations

---
+++ portfolio/gatsby-browser.js
@@ -0,0 +1,13 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+export const onInitialClientRender = () => {
+
+    document.body.setAttribute('id', 'body');
+    document.body.setAttribute('data-aos', 'body');
+
+    AOS.init({
+        once: true,
+        duration: 1200,
+    });
+};
--- portfolio/src/components/Layout.js
+++ portfolio/src/components/Layout.js
@@ -1,21 +1,10 @@
-import React, {useEffect} from "react"
+import React from "react"
 import * as styles from '../css/styles.css'
 import "@fortawesome/fontawesome-free/css/all.css" // import "@fortawesome/fontawesome-free/css/all.css"
 import Header from "./Header";
-import AOS from "aos";
+
 
 export default function Layout({ children }) {
-    document.body.setAttribute('id', 'body');
-    document.body.setAttribute('data-aos', 'body');
-
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            AOS.init({
-                once: true,
-                duration: 1200
-            })
-        }
-    })
 
     return (
         <div className="layout">
